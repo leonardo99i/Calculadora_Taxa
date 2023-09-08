9de819a52a6966dbd28c3884ce7567762261d48f
Leonardo R. Leonardi <49618344+leonardo99i@users.noreply.github.com>
2023-09-08 03:23:58 -0300

    Ajuste no CSS, e revendo o calculo do imposto

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ async function obterTaxaCambioDolar() {
     try {
         const resposta = await fetch('https://economia.awesomeapi.com.br/json/USD-BRL');
         const dados = await resposta.json();
-        return dados[0].bid;
+        return parseFloat(dados[0].bid);
     } catch (erro) {
         console.error('Erro ao obter a taxa de câmbio do dólar:', erro);
         return null;
@@ -11,7 +11,7 @@ async function obterTaxaCambioDolar() {
 }
 
 // Função para calcular o imposto
-function calcularImposto() {
+async function calcularImposto() {
     const valorCompraBRL = parseFloat(document.getElementById("valorCompra").value);
     const valorFreteBRL = parseFloat(document.getElementById("valorFrete").value);
 
@@ -20,33 +20,37 @@ function calcularImposto() {
         return;
     }
 
-    // Converter valores de BRL para dólares usando a taxa de câmbio
-    const taxaCambio = 5; // Taxa de câmbio fixada em 5 reais por dólar
-    const valorCompraUSD = valorCompraBRL / taxaCambio;
-    const valorFreteUSD = valorFreteBRL / taxaCambio;
-    
-    const valorTotalUSD = valorCompraUSD + valorFreteUSD;
+    // Obter a taxa de câmbio mais recente
+    const taxaCambio = await obterTaxaCambioDolar();
 
-    let imposto;
-    if (valorTotalUSD <= 50) {
-        // Imposto de 17% (ICMS) sobre o valor total em reais
-        imposto = (valorCompraBRL + valorFreteBRL) * 0.17;
+    if (taxaCambio !== null) {
+        const valorCompraUSD = valorCompraBRL / taxaCambio;
+        const valorFreteUSD = valorFreteBRL / taxaCambio;
+        const valorTotalUSD = valorCompraUSD + valorFreteUSD;
+
+        let imposto;
+        if (valorTotalUSD <= 50) {
+            // Imposto de 17% (ICMS) sobre o valor total em reais
+            imposto = (valorCompraBRL + valorFreteBRL) * 0.17;
+        } else {
+            // Imposto de 17% (ICMS) sobre o valor total em reais
+            imposto = (valorCompraBRL + valorFreteBRL) * 0.17;
+
+            // Taxa adicional de 60% sobre o valor em reais após o ICMS
+            imposto += (valorCompraBRL + valorFreteBRL + imposto) * 0.60;
+        }
+
+        // Valor total da compra com imposto em reais
+        const valorTotalCompraComImpostoBRL = valorCompraBRL + valorFreteBRL + imposto;
+
+        const elementoResultado = document.getElementById("resultado");
+        elementoResultado.textContent = `Imposto de Importação em BRL: R$ ${(imposto).toFixed(2)}`;
+
+        const elementoValorComImposto = document.getElementById("valorComImposto");
+        elementoValorComImposto.value = valorTotalCompraComImpostoBRL.toFixed(2);
     } else {
-        // Imposto de 17% (ICMS) sobre o valor total em reais
-        imposto = (valorCompraBRL + valorFreteBRL) * 0.17;
-        
-        // Taxa adicional de 60% sobre o valor em reais após o ICMS
-        imposto += (valorCompraBRL + valorFreteBRL + imposto) * 0.60;
+        alert("Não foi possível obter a taxa de câmbio. Verifique a conexão com a internet.");
     }
-
-    // Valor total da compra com imposto em reais
-    const valorTotalCompraComImpostoBRL = valorCompraBRL + valorFreteBRL + imposto;
-
-    const elementoResultado = document.getElementById("resultado");
-    elementoResultado.textContent = `Imposto de Importação em BRL: R$ ${(imposto).toFixed(2)}`;
-
-    const elementoValorComImposto = document.getElementById("valorComImposto");
-    elementoValorComImposto.value = valorTotalCompraComImpostoBRL.toFixed(2);
 
     atualizarDataECotacao();
 }
